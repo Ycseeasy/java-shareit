5a73ae06ff12d8fbbcbb19d2471df9ebe32d0726
Ycseeasy <ycse2easyy@yandex.ru>
2024-10-02 13:11:44 +0300

    Исправление замечаний от Ревьюера

--- server/src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -6,7 +6,6 @@ import ru.practicum.shareit.item.model.Comment;
 import java.util.Collection;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    
     Collection<Comment> findByItemId(long itemId);
 
     Collection<Comment> findByItemIdIn(Collection<Long> itemIds);
